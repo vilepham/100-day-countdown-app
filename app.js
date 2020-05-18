789350f08debb3e25781345a7030639cbcd8aa62
vilepham <vilepham136@gmail.com>
2020-05-19 03:26:37 +0900

    Rewritten the entire js

--- app.js
+++ app.js
@@ -5,30 +5,26 @@ document.addEventListener('DOMContentLoaded', () => {
         hour = minute * 60,
         day = hour * 24;
 
-    const clock = document.querySelector('.countdown');
     const title = document.querySelector('.title');
     const timer = document.querySelector('.timer');
-    let date = document.querySelector('.deadline');
-    let deadline = document.querySelector('#change-dl');
 
-
-    let countDown = new Date('Aug 3, 2020 23:59:59').getTime(),
-        x = setInterval(function() {    
+    let deadline = new Date('Aug 3, 2020 23:59:59').getTime(),
+        x = setInterval(function() {
 
         let now = new Date().getTime(),
-            distance = countDown - now;
+            timeLeft = deadline - now;
 
-            document.getElementById('days').innerText = Math.floor(distance / (day)),
-            document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-            document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
-            document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+            document.getElementById('days').innerText = Math.floor(timeLeft / (day)), 
+            document.getElementById('hours').innerText = Math.floor((timeLeft % (day)) / (hour)), 
+            document.getElementById('minutes').innerText = Math.floor((timeLeft % (hour)) / (minute)), 
+            document.getElementById('seconds').innerText = Math.floor((timeLeft % (minute)) / second); 
 
-            if (distance < 0) {
+            if (timeLeft < 0) {
                 clearInterval(x);
                 clock.removeChild('timer');
                 title.innerHTML= 'Are you Ready?';
             };
-        
+
         },second);
 
     });
